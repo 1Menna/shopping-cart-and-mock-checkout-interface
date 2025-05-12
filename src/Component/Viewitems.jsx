44dac031325b36c0348a218c8d0612191ac31d0d
1Menna <meno903010@gmail.com>
2025-05-12 19:02:08 +0300

    -

--- src/Component/Viewitems.jsx
+++ src/Component/Viewitems.jsx
@@ -2,25 +2,24 @@ import React from 'react'
 
 const Viewitems = ({product , increment , decrement, Delete}) => {
     return (
-        //============================================
-        <div className="card bg-gray-400 w-80">
-            <figure>
-                <img src={product.src} alt="Shoes" />
-            </figure>
-            <div className="card-body text-center">
-                <div className='text-3xl text-center '>Product {product.id}</div>
-                <p className='text-red-500'>Count : {product.count} </p>
-                <p > price : {product.price}$ </p>
-                <p className='text-3xl'>SubTotal : {product.subtotal}$ </p>
-                <br />
-                <div className="flex justify-between">
-                     <button className='btn bg-success' onClick={()=>increment(product.id)}>Add</button>               
-                     <button className='btn bg-warning' onClick={()=>decrement(product.id)}>remove</button>               
-                     <button className='btn bg-error' onClick={()=>Delete(product.id)}>Del</button>               
-                </div>
-            </div>
+         //============================================
+<div className="card bg-gray-400 w-60"> {/* Reduced width from w-80 to w-60 */}
+    <figure>
+        <img src={product.src} alt="Shoes" className="h-32 object-cover" /> {/* Added fixed height */}
+    </figure>
+    <div className="card-body text-center p-4"> {/* Added padding control */}
+        <div className='text-xl text-center'>Product {product.id}</div> {/* Reduced text size */}
+        <p className='text-red-500 text-sm'>Count : {product.count} </p> {/* Smaller text */}
+        <p className='text-sm'> price : {product.price}$ </p> {/* Smaller text */}
+        <p className='text-xl'>SubTotal : {product.subtotal}$ </p> {/* Reduced from text-3xl */}
+        <div className="flex justify-between mt-2"> {/* Added margin top */}
+            <button className='btn bg-success btn-sm' onClick={()=>increment(product.id)}>Add</button> {/* Added btn-sm */}           
+            <button className='btn bg-warning btn-sm' onClick={()=>decrement(product.id)}>remove</button> {/* Added btn-sm */}           
+            <button className='btn bg-error btn-sm' onClick={()=>Delete(product.id)}>Del</button> {/* Added btn-sm */}           
         </div>
-        // =============================================
+    </div>
+</div>
+// =============================================
     )
 }
 
